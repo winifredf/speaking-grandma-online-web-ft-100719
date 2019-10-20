0acbbe53d6781f3ab33bd0aaca2eaa5a35b28231
Winifred Frierson <winifred.dykes@hotmail.com>
2019-10-20 23:30:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,22 @@
-# Write a speak_to_grandma method.
+speak_to_grandma = "I love you grandma!"
+
+  if  speak_to_grandma == "I LOVE YOU GRANDMA!"
+    puts "HI, GRANDMA!"
+  
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+end
+
+  if  speak_to_grandma == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  
+  else  "what did you say?"
+end
+
+
+
+    
+    # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
