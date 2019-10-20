92246330e2cba06236f1f1ac4c33dace07b947a3
Winifred Frierson <winifred.dykes@hotmail.com>
2019-10-20 23:45:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ speak_to_grandma = "I love you grandma!"
   else puts "HUH?! SPEAK UP, SONNY!"
 end
   
-speak_to_grandma == "WHEN WERE YOU BORN?"
+puts speak_to_grandma == "WHEN WERE YOU BORN?"
 
   if  speak_to_grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
